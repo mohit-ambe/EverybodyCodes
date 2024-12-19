a746d3df5caaedd06e1351f5484405592b5ac276
Mohit Ambe <mambe@purdue.edu>
2024-12-18 22:34:11 -0500

    Day 1

--- 01/solution.py
+++ 01/solution.py
@@ -4,15 +4,45 @@ dataset = [tuple(line.strip() for line in file.readlines()) for file in files]
 
 
 def part1(data=dataset[0]):
-    print()
+    potions = 0
+    for battle in data[0]:
+        if battle == "B":
+            potions += 1
+        elif battle == "C":
+            potions += 3
+    print(potions)
 
 
 def part2(data=dataset[1]):
-    print()
+    potions = 0
+    for pair in [data[0][i - 2:i] for i in range(2, len(data[0]) + 1, 2)]:
+        for battle in pair:
+            if battle == "B":
+                potions += 1
+            elif battle == "C":
+                potions += 3
+            elif battle == "D":
+                potions += 5
+        if "x" not in pair:
+            potions += 2
+    print(potions)
 
 
 def part3(data=dataset[2]):
-    print()
+    potions = 0
+    for trio in [data[0][i - 3:i] for i in range(3, len(data[0]) + 1, 3)]:
+        for battle in trio:
+            if battle == "B":
+                potions += 1
+            elif battle == "C":
+                potions += 3
+            elif battle == "D":
+                potions += 5
+        if "x" not in trio:
+            potions += 6
+        elif trio.count("x") == 1:
+            potions += 2
+    print(potions)
 
 
 part1()
