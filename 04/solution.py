1616d3a1752151ab28b3b4beefe6597d6a9f1365
Mohit Ambe <mambe@purdue.edu>
2024-12-19 19:38:54 -0500

    Day 4

--- 04/solution.py
+++ 04/solution.py
@@ -3,16 +3,20 @@ dataset = [tuple(line.strip() for line in file.readlines()) for file in files]
 [f.close() for f in files]
 
 
-def part1(data=dataset[0]):
-    print()
+def part1(data=dataset[0], p3=False):
+    nails = [int(x) for x in data]
+    level = (sorted(nails)[len(nails) // 2] if p3 else min(nails))
+    print(sum([abs(n - level) for n in nails]))
 
 
 def part2(data=dataset[1]):
-    print()
+    part1(data)
 
 
 def part3(data=dataset[2]):
-    print()
+    # the deviations between values in a sample
+    # are minimized at the median value
+    part1(data, p3=True)
 
 
 part1()
