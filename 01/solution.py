2e80105f91305daa9e0579a6977b05e4160bdbfb
Mohit Ambe <mambe@purdue.edu>
2025-11-14 10:57:52 -0500

    Day 1

--- 01/solution.py
+++ 01/solution.py
@@ -4,15 +4,34 @@ dataset = [tuple(line.strip() for line in file.readlines()) for file in files]
 
 
 def part1(data=dataset[0]):
-    print()
+    names = data[0].split(",")
+    dirs = [[d[0], d[1:]] for d in data[2].split(",")]
+    i = 0
+    for d, x in dirs:
+        i += (1 if d == "R" else -1) * int(x)
+        i = min(max(i, 0), len(names) - 1)
+
+    print(names[i])
 
 
 def part2(data=dataset[1]):
-    print()
+    names = data[0].split(",")
+    dirs = [[d[0], d[1:]] for d in data[2].split(",")]
+    i = 0
+    for d, x in dirs:
+        i += (1 if d == "R" else -1) * int(x)
+
+    print(names[i % len(names)])
 
 
 def part3(data=dataset[2]):
-    print()
+    names = data[0].split(",")
+    dirs = [[d[0], d[1:]] for d in data[2].split(",")]
+    for d, x in dirs:
+        i = (1 if d == "R" else -1) * int(x) % len(names)
+        names[0], names[i] = names[i], names[0]
+
+    print(names[0])
 
 
 part1()
