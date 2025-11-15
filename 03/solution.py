aa2c86c184d5f56ff352b7a0af5c156b5f2865f9
Mohit Ambe <mohitambe77@gmail.com>
2025-11-14 22:54:23 -0500

    Day 3

--- 03/solution.py
+++ 03/solution.py
@@ -4,15 +4,38 @@ dataset = [tuple(line.strip() for line in file.readlines()) for file in files]
 
 
 def part1(data=dataset[0]):
-    print()
+    crates = sorted([int(x) for x in data[0].split(",")], reverse=True)
+    prev = 1e10
+    sum = 0
+    i = 0
+    while i < len(crates):
+        if crates[i] < prev:
+            sum += crates[i]
+            prev = crates[i]
+        i += 1
+    print(sum)
 
 
 def part2(data=dataset[1]):
-    print()
+    crates = sorted([int(x) for x in data[0].split(",")])
+    prev = 0
+    sum = 0
+    count = 0
+    i = 0
+    while i < len(crates) and count < 20:
+        if crates[i] > prev:
+            sum += crates[i]
+            prev = crates[i]
+            count += 1
+        i += 1
+    print(sum)
 
 
 def part3(data=dataset[2]):
-    print()
+    # crate sequences can contain a size n only once
+    # so, the minimum sets is the maximum frequency of a size n
+    crates = data[0].split(",")
+    print(max([crates.count(c) for c in set(crates)]))
 
 
 part1()
